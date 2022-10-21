00ac6f9f5333aa186a070adcdf64d7c2c6e120ac
ahmed benchekroun <ahmedbenchekroun123@gmail.com>
2022-10-21 11:10:42 -0400

    features completed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
+    if user_signed_in?
+      redirect_to friends_path
+    else
+      redirect_to new_user_registration_path
+    end
   end
 
   def about
